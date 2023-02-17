8e1f448651a60ada2f5aff1e2a672263e934fca6
Abir <abirovi481@gmail.com>
2023-02-18 00:53:05 +0600

    sixth commit

--- js/color-change.js
+++ js/color-change.js
@@ -1,7 +1,8 @@
+// mouseover
 document.getElementById("box-1").addEventListener("mouseover", function(){
     document.getElementById("box-1").style.backgroundColor = randomColor();
 });
-  
+
 document.getElementById("box-2").addEventListener("mouseover", function(){
     document.getElementById("box-2").style.backgroundColor = randomColor();
 });
@@ -21,4 +22,22 @@ document.getElementById("box-5").addEventListener("mouseover", function(){
 document.getElementById("box-6").addEventListener("mouseover", function(){
     document.getElementById("box-6").style.backgroundColor = randomColor();
 });
-  
+// mouse out 
+document.getElementById("box-1").addEventListener("mouseout", function(){
+    document.getElementById("box-1").style.backgroundColor = "white";
+});
+document.getElementById("box-2").addEventListener("mouseout", function(){
+    document.getElementById("box-2").style.backgroundColor = "white";
+});
+document.getElementById("box-3").addEventListener("mouseout", function(){
+    document.getElementById("box-3").style.backgroundColor = "white";
+});
+document.getElementById("box-4").addEventListener("mouseout", function(){
+    document.getElementById("box-4").style.backgroundColor = "white";
+});
+document.getElementById("box-5").addEventListener("mouseout", function(){
+    document.getElementById("box-5").style.backgroundColor = "white";
+});
+document.getElementById("box-6").addEventListener("mouseout", function(){
+    document.getElementById("box-6").style.backgroundColor = "white";
+});
